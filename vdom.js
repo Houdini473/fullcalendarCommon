d87d57bfb871c1ef2de71a0006d3de3e2ed550aa
Houdini473 <72358779+Houdini473@users.noreply.github.com>
2021-04-09 22:01:26 -0700

    gitignore

--- vdom.js
+++ vdom.js
@@ -1,6 +1,8 @@
-/// <reference types="@fullcalendar/core-preact" />
-if (typeof FullCalendarVDom === 'undefined') {
-    throw new Error('Please import the top-level fullcalendar lib before attempting to import a plugin.');
+// / <reference types="@fullcalendar/core-preact" />
+if (typeof FullCalendarVDom === "undefined") {
+  throw new Error(
+    "Please import the top-level fullcalendar lib before attempting to import a plugin."
+  );
 }
 var Component = FullCalendarVDom.Component;
 var createElement = FullCalendarVDom.createElement;
@@ -11,4 +13,13 @@ var createContext = FullCalendarVDom.createContext;
 var flushToDom = FullCalendarVDom.flushToDom;
 var unmountComponentAtNode = FullCalendarVDom.unmountComponentAtNode;
 
-export { Component, Fragment, createContext, createElement, createRef, flushToDom, render, unmountComponentAtNode };
+export {
+  Component,
+  Fragment,
+  createContext,
+  createElement,
+  createRef,
+  flushToDom,
+  render,
+  unmountComponentAtNode,
+};
